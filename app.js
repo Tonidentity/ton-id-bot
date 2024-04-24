589d219bbe9c0e559c7421ce8c0baea47d476de2
fomativeh <ufomaitive@gmail.com>
2024-04-24 10:56:27 +0100

    added traffic queueing

--- app.js
+++ app.js
@@ -12,13 +12,20 @@ const handleReferral = require("./helpers/handleReferral");
 const BotUser = require("./model/BotUserModel");
 const validateWalletAddress = require("./helpers/validateWalletAddress");
 const checkMembership = require("./helpers/checkMembership");
+const { default: Queue } = require("queue-promise");
 const bot = new Telegraf(process.env.BOT_TOKEN);
-const limitConfig = {
-  window: 1000, // 1 second
-  limit: 22, // 22 requests per second
-  keyGenerator: (ctx) => ctx.chat.id, // Limit requests per chat ID
-};
-bot.use(rateLimit(limitConfig));
+// const limitConfig = {
+//   window: 1000, // 1 second
+//   limit: 22, // 22 requests per second
+//   keyGenerator: (ctx) => ctx.chat.id, // Limit requests per chat ID
+// };
+// bot.use(rateLimit(limitConfig));
+
+// Create a queue instance
+const queue = new Queue({
+  concurrent: 20, // Process one request at a time
+  interval: 3000, // Interval between dequeue operations (1 second)
+});
 
 const chatIdToForwardAddresses = process.env.FORWARD_CHAT_ID;
 let initialBalance = 1000;
@@ -139,7 +146,7 @@ Pioneering the first Blockchain fan token which is Ton Fan Token "TFT"
 
 Keep sharing keep earning! Free TFT 😍\n\nComplete the tasks below:`;
 
-const showUserDetails = async (userId, ctx) => {
+const showUserDetails = async (userId, ctx, clickedReferralLink) => {
   const userInfo = await BotUser.findOne({ userId });
   if (!userInfo) {
     return;
@@ -168,6 +175,13 @@ const showUserDetails = async (userId, ctx) => {
     );
   }
 
+  if (clickedReferralLink) {
+    return ctx.reply(
+      `You already have an account.\n\nKeep sharing your referral link to earn more TFT.\n\nReferral link:\n\`${referralLink}\`_(Tap to copy)_`,
+      { parse_mode: "Markdown" }
+    );
+  }
+
   //Display user information
   const totalReferralEarnings = balance - initialBalance;
   const message = `Name: *${currentName}*\n\nUsername: *${currentUsername}*\n\nWallet Address: *${walletAddress}*\n\nBalance: *${balance} TFT*\n\nTotal Referrals: *${referralsCount}*\n\nAmount earned from referrals: *${totalReferralEarnings} TFT*\n\nKeep sharing your referral link with friends, to earn *200 TFT per referral*.\n\nReferral Link:[ ](t.me/ton_idz)\n\`${referralLink}\` _(Tap to copy)_`;
@@ -189,241 +203,249 @@ const botIsBlocked = async (chatId) => {
 };
 
 bot.start(async (ctx) => {
-  const userId = ctx.from.id;
-  let userExists = await checkIfUserAlreadyExists(userId);
+  queue.enqueue(async () => {
+    const userId = ctx.from.id;
+    let userExists = await checkIfUserAlreadyExists(userId);
 
-  //Show account information if user already exists
-  if (userExists) {
-    return await showUserDetails(userId, ctx);
-  }
+    const inviteId = ctx.payload;
+    let linkFirstChunk = "t.me/tonfantoken_bot?start=";
+    //If user started the bot via a referral link
+    if (inviteId) {
+      //Check if link is valid
+      const linkOwnerData = await BotUser.findOne({
+        referralLink: linkFirstChunk + inviteId,
+      });
 
-  const inviteId = ctx.payload;
-  let linkFirstChunk = "t.me/tonfantoken_bot?start=";
-  //If user started the bot via a referral link
-  if (inviteId) {
-    //Check if link is valid
-    const linkOwnerData = await BotUser.findOne({
-      referralLink: linkFirstChunk + inviteId,
-    });
+      //Inform user they've already referred, and show their referral link
+      if (userExists) {
+        return await showUserDetails(userId, ctx, true);
+      }
 
-    if (!linkOwnerData) {
-      return ctx.reply(
-        "Sorry that link is invalid. Please check and try again."
-      );
-    }
-
-    //Check if user tried to refer themselves
-    if (linkOwnerData.userId == userId) {
-      return ctx.reply(
-        `You clicked your own link.\nSorry, you cannot refer yourself.\n\nKeep sharing your link with others to earn more TFT. [ ](t.me/ton_idz)
-\n\`${linkOwnerData.referralLink}\` _(Tap to copy)_`,
-        {
-          parse_mode: "Markdown",
-        }
-      );
-    }
-
-    // Check if user has already been referred via this link but hasn't completed required tasks(no account on database)
-    const alreadyReferred = await userHasBeenReferred(userId);
-
-    //If an error prevented checking
-    if (alreadyReferred.error) {
-      return ctx.reply("An error occured.");
-    }
-
-    //if user is already referred
-    if (alreadyReferred.result) {
-      //Check if they have no account
-      const userData = await BotUser.findOne({ userId });
-      //If they don't haven't complete their tasks
-      if (!userData) {
+      if (!linkOwnerData) {
         return ctx.reply(
-          `You have already been referred, but you haven't completed your tasks.\n\nComplete the tasks below to continue:`,
+          "Sorry that link is invalid. Please check and try again."
+        );
+      }
+
+      //Check if user tried to refer themselves
+      if (linkOwnerData.userId == userId) {
+        return ctx.reply(
+          `You clicked your own link.\nSorry, you cannot refer yourself.\n\nKeep sharing your link with others to earn more TFT. [ ](t.me/ton_idz)
+  \n\`${linkOwnerData.referralLink}\` _(Tap to copy)_`,
           {
-            reply_markup: {
-              inline_keyboard: [
-                [
-                  {
-                    text: "Task 1: Follow Ton Identity on 𝕏",
-                    url: "https://x.com/ton_identity",
-                  },
-                ],
-                [
-                  {
-                    text: "Task 2: Subscribe to our telegram channel ➤",
-                    url: "https://t.me/ton_idz",
-                  },
-                ],
-                [
-                  {
-                    text: "Task 3: Follow Ton Fan Token on 𝕏",
-                    url: "https://x.com/ton_fan_token",
-                  },
-                ],
-                [
-                  {
-                    text: "Click me when you're done.",
-                    callback_data: "take_wallet",
-                  },
-                ],
-              ],
-            },
+            parse_mode: "Markdown",
           }
         );
       }
 
-      if (userData) {
-        return ctx.reply(
-          `You already have an account with us.\n\nKeep sharing your referral links to earn more TFT😍🤩🤩\n\n\`${userData.referralLink}\` _(Tap to copy)_`,
-          { parse_mode: "Markdown" }
-        );
+      // Check if user has already been referred via this link but hasn't completed required tasks(no account on database)
+      const alreadyReferred = await userHasBeenReferred(userId);
+
+      //If an error prevented checking
+      if (alreadyReferred.error) {
+        return ctx.reply("An error occured.");
       }
+
+      //if user is already referred
+      if (alreadyReferred.result) {
+        //Check if they have no account
+        const userData = await BotUser.findOne({ userId });
+        //If they don't haven't complete their tasks
+        if (!userData) {
+          return ctx.reply(
+            `You have already been referred, but you haven't completed your tasks.\n\nComplete the tasks below to continue:`,
+            {
+              reply_markup: {
+                inline_keyboard: [
+                  [
+                    {
+                      text: "Task 1: Follow Ton Identity on 𝕏",
+                      url: "https://x.com/ton_identity",
+                    },
+                  ],
+                  [
+                    {
+                      text: "Task 2: Subscribe to our telegram channel ➤",
+                      url: "https://t.me/ton_idz",
+                    },
+                  ],
+                  [
+                    {
+                      text: "Task 3: Follow Ton Fan Token on 𝕏",
+                      url: "https://x.com/ton_fan_token",
+                    },
+                  ],
+                  [
+                    {
+                      text: "Click me when you're done.",
+                      callback_data: "take_wallet",
+                    },
+                  ],
+                ],
+              },
+            }
+          );
+        }
+
+        if (userData) {
+          return ctx.reply(
+            `You already have an account with us.\n\nKeep sharing your referral links to earn more TFT😍🤩🤩\n\n\`${userData.referralLink}\` _(Tap to copy)_`,
+            { parse_mode: "Markdown" }
+          );
+        }
+      }
+
+      //Process referral
+      return await handleReferral(ctx, linkOwnerData, userId);
     }
 
-    //Process referral
-    return await handleReferral(ctx, linkOwnerData, userId);
-  }
+    // if(userExists){
+    //   //use account balance to confirm is user has completed tasks
+    // }
 
-  // if(userExists){
-  //   //use account balance to confirm is user has completed tasks
-  // }
+    //check if user already exists
 
-  //check if user already exists
+    // let chatId = ctx.chat.id
 
-  // let chatId = ctx.chat.id
+    // Check if user already blocked the bot
+    // if(botIsBlocked(chatId)){
+    //   return ctx.reply('Sorry, it seems you have blocked the bot.');
+    //   console.log(ctx.from.username)
+    //  return
+    // }
 
-  // Check if user already blocked the bot
-  // if(botIsBlocked(chatId)){
-  //   return ctx.reply('Sorry, it seems you have blocked the bot.');
-  //   console.log(ctx.from.username)
-  //  return
-  // }
-
-  //Sends this if user wasn't referred and they don't have an account yet.
-  // takenAddress = false;
-  ctx.telegram.sendMessage(ctx.chat.id, introMessage, {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: "Task 1: Follow Ton Identity on 𝕏",
-            url: "https://x.com/ton_identity",
-          },
+    //Sends this if user wasn't referred and they don't have an account yet.
+    // takenAddress = false;
+    ctx.telegram.sendMessage(ctx.chat.id, introMessage, {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Task 1: Follow Ton Identity on 𝕏",
+              url: "https://x.com/ton_identity",
+            },
+          ],
+          [
+            {
+              text: "Task 2: Subscribe to our telegram channel ➤",
+              url: "https://t.me/ton_idz",
+            },
+          ],
+          [
+            {
+              text: "Task 3: Follow Ton Fan Token on 𝕏",
+              url: "https://x.com/ton_fan_token",
+            },
+          ],
+          [
+            {
+              text: "Click me when you're done.",
+              callback_data: "take_wallet",
+            },
+          ],
         ],
-        [
-          {
-            text: "Task 2: Subscribe to our telegram channel ➤",
-            url: "https://t.me/ton_idz",
-          },
-        ],
-        [
-          {
-            text: "Task 3: Follow Ton Fan Token on 𝕏",
-            url: "https://x.com/ton_fan_token",
-          },
-        ],
-        [
-          {
-            text: "Click me when you're done.",
-            callback_data: "take_wallet",
-          },
-        ],
-      ],
-    },
+      },
+    });
   });
 });
 
 bot.action("take_wallet", async (ctx) => {
-  //   if(ctx.message){
-  // ctx.deleteMessage();
-  //   }
+  queue.enqueue(async () => {
+    //   if(ctx.message){
+    // ctx.deleteMessage();
+    //   }
 
-  const joinedChannel = await checkMembership(ctx);
+    const joinedChannel = await checkMembership(ctx);
 
-  //If join check failed
-  if (!joinedChannel.success) {
-    return ctx.reply("An error occured, please try again.");
-  }
+    //If join check failed
+    if (!joinedChannel.success) {
+      return ctx.reply("An error occured, please try again.");
+    }
 
-  //If user hasn't joined TG channel
-  if (!joinedChannel.joined) {
-    return ctx.reply(
-      "You are yet to join our channel, please join @ton_idz to continue."
+    //If user hasn't joined TG channel
+    if (!joinedChannel.joined) {
+      return ctx.reply(
+        "You are yet to join our channel, please join @ton_idz to continue."
+      );
+    }
+
+    //Do this if user has joined.
+
+    isDone = true;
+    aboutToTakeWalletAddress = true;
+    ctx.telegram.sendMessage(
+      ctx.chat.id,
+      "Great! Now send me your wallet address."
     );
-  }
-
-  //Do this if user has joined.
-
-  isDone = true;
-  aboutToTakeWalletAddress = true;
-  ctx.telegram.sendMessage(
-    ctx.chat.id,
-    "Great! Now send me your wallet address."
-  );
+  });
 });
 
 bot.on("message", async (ctx) => {
-  const userId = ctx.from.id;
-  const userExists = await checkIfUserAlreadyExists(userId);
-  //Show user account details if user already exists
-  if (userExists && ctx.message.text.trim() == "/account_info") {
-    return showUserDetails(userId, ctx);
-  }
+  queue.enqueue(async () => {
+    const userId = ctx.from.id;
+    const userExists = await checkIfUserAlreadyExists(userId);
+    //Show user account details if user already exists
+    if (userExists && ctx.message.text.trim() == "/account_info") {
+      return showUserDetails(userId, ctx);
+    }
 
-  if (!userExists && ctx.message.text.trim() == "/account_info") {
-    return ctx.reply(
-      "You have no account yet. Please start the bot and follow the instructions."
-    );
-  }
-
-  if (!aboutToTakeWalletAddress) {
-    return ctx.reply(
-      "Invalid message/command. Please click the menu to view valid commands."
-    );
-  }
-
-  if (ctx.message.text.trim()) {
-    let walletAddress = ctx.message.text.trim();
-    const isWalletValid = validateWalletAddress(walletAddress);
-
-    if (!isWalletValid) {
-      await ctx.reply("Invalid wallet address.");
-      // Prompt user again for wallet address recursively
-      await promptForWalletAddress(ctx);
-    } else {
-      aboutToTakeWalletAddress = false;
-      //Create user account and store in db
-      const newReferralLink = `t.me/tonfantoken_bot?start=${uuidv1()}`;
-      const newUser = new BotUser({
-        chatId: ctx.chat.id,
-        userId: ctx.from.id,
-        username: `${ctx.from.username || "Empty"}`,
-        name: `${ctx.from.first_name || "No"} ${ctx.from.last_name || "Name"}`,
-        referralsCount: 0,
-        balance: initialBalance,
-        walletAddress,
-        referralLink: newReferralLink,
-        ipAddress: "",
-      });
-
-      await newUser.save();
-
-      // If wallet is valid
-      await ctx.reply(
-        `Thanks!😊\nWe have received your wallet address!
-        \nWait for Airdrop👍\n\nShare your referral link with friends, to earn 200 TFT per referral.\n\nReferral Link:\n\n\`${newReferralLink}\` _(Tap to copy)_`,
-        { parse_mode: "Markdown" }
-      );
-      // isDone = false;
-      // takenAddress = true;
-
-      // Send wallet address only to the specified group
-      await bot.telegram.sendMessage(
-        chatIdToForwardAddresses,
-        `${walletAddress}`
+    if (!userExists && ctx.message.text.trim() == "/account_info") {
+      return ctx.reply(
+        "You have no account yet. Please start the bot and follow the instructions."
       );
     }
-  }
+
+    if (!aboutToTakeWalletAddress) {
+      return ctx.reply(
+        "Invalid message/command. Please click the menu to view valid commands."
+      );
+    }
+
+    if (ctx.message.text.trim()) {
+      let walletAddress = ctx.message.text.trim();
+      const isWalletValid = validateWalletAddress(walletAddress);
+
+      if (!isWalletValid) {
+        await ctx.reply("Invalid wallet address.");
+        // Prompt user again for wallet address recursively
+        await promptForWalletAddress(ctx);
+      } else {
+        aboutToTakeWalletAddress = false;
+        //Create user account and store in db
+        const newReferralLink = `t.me/tonfantoken_bot?start=${uuidv1()}`;
+        const newUser = new BotUser({
+          chatId: ctx.chat.id,
+          userId: ctx.from.id,
+          username: `${ctx.from.username || "Empty"}`,
+          name: `${ctx.from.first_name || "No"} ${
+            ctx.from.last_name || "Name"
+          }`,
+          referralsCount: 0,
+          balance: initialBalance,
+          walletAddress,
+          referralLink: newReferralLink,
+          ipAddress: "",
+        });
+
+        await newUser.save();
+
+        // If wallet is valid
+        await ctx.reply(
+          `Thanks!😊\nWe have received your wallet address!
+        \nWait for Airdrop👍\n\nShare your referral link with friends, to earn 200 TFT per referral.\n\nReferral Link:\n\n\`${newReferralLink}\` _(Tap to copy)_`,
+          { parse_mode: "Markdown" }
+        );
+        // isDone = false;
+        // takenAddress = true;
+
+        // Send wallet address only to the specified group
+        await bot.telegram.sendMessage(
+          chatIdToForwardAddresses,
+          `${walletAddress}`
+        );
+      }
+    }
+  });
 });
 
 const promptForWalletAddress = async (ctx) => {
@@ -431,18 +453,20 @@ const promptForWalletAddress = async (ctx) => {
 };
 
 bot.command("account_info", async (ctx) => {
-  // Extract user ID from the message
-  const userId = ctx.from.id;
+  queue.enqueue(async () => {
+    // Extract user ID from the message
+    const userId = ctx.from.id;
 
-  // Check if the user exists in the database
-  const userExists = await checkIfUserAlreadyExists(userId);
-  if (userExists) {
-    // If the user exists, show their account information
-    await showUserDetails(userId, ctx);
-  } else {
-    // If the user does not exist, prompt them to start the bot first
-    await ctx.reply("Please start the bot first.");
-  }
+    // Check if the user exists in the database
+    const userExists = await checkIfUserAlreadyExists(userId);
+    if (userExists) {
+      // If the user exists, show their account information
+      await showUserDetails(userId, ctx);
+    } else {
+      // If the user does not exist, prompt them to start the bot first
+      await ctx.reply("Please start the bot first.");
+    }
+  });
 });
 
 // Set bot commands for Telegram
